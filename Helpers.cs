7042e51fbf2d66c0c675ba23dce2043ad38fb918
Jagget <jagget@mail.ru>
2024-04-07 00:53:38 -0400

    v1.10.1: PlayerHandsover and PlayerPossesses count items fix

--- Helpers.cs
+++ Helpers.cs
@@ -10,33 +10,46 @@ namespace Game.Mods.QuestActionsExtension
         public static int CalculateLengthOrStackCount(IReadOnlyCollection<DaggerfallUnityItem> itemList)
         {
             if (itemList == null) return 0;
+            var count = 0;
 
-            foreach (var item in itemList.Where(item => !item.IsSummoned && item.IsAStack()))
+            foreach (var item in itemList.Where(item => !item.IsSummoned))
             {
-                return item.stackCount;
+                count += item.IsAStack() ? item.stackCount : 1;
             }
 
-            return itemList.Count;
+            return count;
         }
 
         public static void RemoveNFromList(ItemCollection storage, List<DaggerfallUnityItem> itemList, int amount)
         {
-            var isAStack = itemList[0].IsAStack();
+            var itemsRemoved = 0;
 
-            if (isAStack)
+            foreach (var item in itemList)
             {
-                itemList[0].stackCount -= amount;
+                if (itemsRemoved >= amount)
+                    break;
 
-                if (itemList[0].stackCount == 0)
+                if (item.IsAStack())
                 {
-                    storage.RemoveItem(itemList[0]);
+                    var stackCount = item.stackCount;
+                    if (stackCount <= amount - itemsRemoved)
+                    {
+                        // Remove the entire stack
+                        storage.RemoveItem(item);
+                        itemsRemoved += stackCount;
+                    }
+                    else
+                    {
+                        // Remove part of the stack
+                        item.stackCount -= (amount - itemsRemoved);
+                        itemsRemoved = amount;
+                    }
                 }
-            }
-            else
-            {
-                for (var i = 0; i < amount; i++)
+                else
                 {
-                    storage.RemoveItem(itemList[i]);
+                    // Remove single item
+                    storage.RemoveItem(item);
+                    itemsRemoved++;
                 }
             }
         }
